17091752fd305498e5b6b679f29fd33a154361c3
Jim Pivarski <jpivarski@gmail.com>
2024-08-07 08:58:06 -0500

    added functional and just_a_loop options to Python and Numba

--- speed-tests/using-numba.py
+++ speed-tests/using-numba.py
@@ -2,19 +2,31 @@ import numpy as np
 import numba as nb
 import time
 
+from functools import reduce
+
 data = np.fromfile("data.int32", np.int32)
 
+
 @nb.jit
-def do_everything(data):
+def just_a_loop(data):
     out = 0
     for x in data:
         out += x * x
     return out
 
+
+square = nb.jit(lambda x: x * x)
+add = nb.jit(lambda x, y: x + y)
+
+@nb.jit
+def just_functional(data):
+    return reduce(add, map(square, data))
+
+
 for repeat in range(10):
     start = time.perf_counter()
 
-    result = do_everything(data)
+    result = just_functional(data)
 
     stop = time.perf_counter()
     print(f"{result = } ({stop - start:.8f} seconds)")
--- speed-tests/using-python.py
+++ speed-tests/using-python.py
@@ -6,19 +6,24 @@ from operator import add
 
 data = np.fromfile("data.int32", np.int32).tolist()
 
-def do_everything(data):
 
+def just_a_loop(data):
     out = 0
     for x in data:
         out += x * x
     return out
 
+
 square = lambda x: x * x
+
+def just_functional(data):
+    return reduce(add, map(square, data))
+
 
 for repeat in range(10):
     start = time.perf_counter()
 
-    result = reduce(add, map(square, data))
+    result = just_functional(data)
 
     stop = time.perf_counter()
     print(f"{result = } ({stop - start:.8f} seconds)")
